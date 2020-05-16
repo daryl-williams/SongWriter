fd3cc247104f14e33d3f6ea23c5e155b0763ca30
Daryl Williams <daryl@weblane.com>
2020-05-16 11:26:29 -0700

    Added file ./ecosystem.config.js for pm2 process management.

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+
+module.exports = {
+  apps : [{
+    name: 'API',
+    script: 'songwriter.js',
+    cwd: '/home/pansq212/projects/songwriter',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    //args: 'one two',
+    instances: 0,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '500M',
+    env: {
+      NODE_ENV: 'production'
+    },
+    env_production: {
+      NODE_ENV: 'development'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'pansq212',
+      host : 'weblane.com:3000',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/home/pansq212/projects/songwriter',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
+
