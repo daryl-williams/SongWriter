8a34645661d1276e3c74ccd7ebcbef586fe9c372
Daryl Williams <daryl@weblane.com>
2020-06-21 14:00:32 -0700

    Work on File menu Export option

--- client/js/jsn/events/select-action.js
+++ client/js/jsn/events/select-action.js
@@ -24,95 +24,95 @@
 import { jsn } from '../index.js';
 
 export function setAction(event) {
-	/*
-	 * This method is called when a change event has occured on the select-action element with id=select-action.
-	 */
-	//console.log('jsn:/client/js/events/select-action.js:setAction(): event =', event);
+  /*
+   * This method is called when a change event has occured on the select-action element with id=select-action.
+   */
+  //console.log('jsn:/client/js/events/select-action.js:setAction(): event =', event);
 
-	event.preventDefault();
+  event.preventDefault();
 
-	if (event.target.classList.contains('disabled')) {
-		return;
-	}
+  if (event.target.classList.contains('disabled')) {
+    return;
+  }
 
-	// We only want to handle file menu actions and not actual song selection
-	// so here we check if the target is song selection or recent file item.
-	if (event.target.classList.contains('song-list-item') || event.target.classList.contains('recent-file-item')) {
-		return;
-	}
+  // We only want to handle file menu actions and not actual song selection
+  // so here we check if the target is song selection or recent file item.
+  if (event.target.classList.contains('song-list-item') || event.target.classList.contains('recent-file-item')) {
+    return;
+  }
 
-	// Close the File menu.
-	let el = event.target.parentElement;
-	el.classList.replace('show', 'hide');
+  // Close the File menu.
+  let el = event.target.parentElement;
+  el.classList.replace('show', 'hide');
 
-	if (event.target.textContent.substring(0, 3) === 'New') {
-		jsn.meta.previous_action = jsn.meta.action;
-		jsn.meta.action = 'new song'
-		jsn.controlPanel.action = 'new';
-		jsn.display.newSong();
-	}
-	else if (event.target.textContent.substring(0, 4) === 'Open') {
-		jsn.meta.previous_action = jsn.meta.action;
-		jsn.meta.action = 'edit';
-		jsn.controlPanel.action = 'edit';
-		jsn.display.fileBrowser();
-	}
-	else if (event.target.textContent.substring(0, 4) === 'Save') {
-		jsn.meta.previous_action = jsn.meta.action;
-		jsn.meta.action = 'save';
-		jsn.controlPanel.action = 'save';
-		jsn.saveSong();
-	}
-	else if (event.target.textContent.substring(0, 5) === 'Print') {
-		jsn.meta.previous_action = jsn.meta.action;
-		jsn.meta.action = 'print';
-		jsn.controlPanel.action = 'print';
+  if (event.target.textContent.substring(0, 3) === 'New') {
+    jsn.meta.previous_action = jsn.meta.action;
+    jsn.meta.action = 'new song'
+    jsn.controlPanel.action = 'new';
+    jsn.display.newSong();
+  }
+  else if (event.target.textContent.substring(0, 4) === 'Open') {
+    jsn.meta.previous_action = jsn.meta.action;
+    jsn.meta.action = 'edit';
+    jsn.controlPanel.action = 'edit';
+    jsn.display.fileBrowser();
+  }
+  else if (event.target.textContent.substring(0, 4) === 'Save') {
+    jsn.meta.previous_action = jsn.meta.action;
+    jsn.meta.action = 'save';
+    jsn.controlPanel.action = 'save';
+    jsn.saveSong();
+  }
+  else if (event.target.textContent.substring(0, 5) === 'Print') {
+    jsn.meta.previous_action = jsn.meta.action;
+    jsn.meta.action = 'print';
+    jsn.controlPanel.action = 'print';
 
-		if (jsn.meta.displayFormat === 'jsn') {
-			jsn.display.jsnFormat();
-		}
-		else if (jsn.meta.displayFormat === 'lmss') {
-			jsn.display.lmssFormat();
-		}
-		else {
-			console.log('jsn:/client/js/events/select-action.js:setAction(): print ERROR: displat format =', event.meta.displayFormat);
-		}
-	}
-	else {
-		if (event.target.id == 'recent-files-opened') {
-			// No harm, no foul. The recent open files event
-			// is handled by the select-song event handler.
-			return;
-		}
-		console.log('jsn:/client/js/events/select-action.js:setAction(): ERROR: unknown file menu action =', event.target.textContent);
-		alert('Unknown File action = ' + event.target.textContent);
-		return;
-	}
+    if (jsn.meta.displayFormat === 'jsn') {
+      jsn.display.jsnFormat();
+    }
+    else if (jsn.meta.displayFormat === 'lmss') {
+      jsn.display.lmssFormat();
+    }
+    else {
+      console.log('jsn:/client/js/events/select-action.js:setAction(): print ERROR: displat format =', event.meta.displayFormat);
+    }
+  }
+  else {
+    if (event.target.id == 'recent-files-opened') {
+      // No harm, no foul. The recent open files event
+      // is handled by the select-song event handler.
+      return;
+    }
+    console.log('jsn:/client/js/events/select-action.js:setAction(): ERROR: unknown file menu action =', event.target.textContent);
+    alert('Unknown File action = ' + event.target.textContent);
+    return;
+  }
 
-	/*
-	//event.target.parentElement.style.color = ":hover";
-	let sheet = document.styleSheets[2];
-	console.log('jsn:/client/js/events/select-action.js:setAction(): sheet =', sheet);
-	let selector = '.dropdown-menu:hover .dropdown-options';
-	let style = 'visibility: visible';
+  /*
+  //event.target.parentElement.style.color = ":hover";
+  let sheet = document.styleSheets[2];
+  console.log('jsn:/client/js/events/select-action.js:setAction(): sheet =', sheet);
+  let selector = '.dropdown-menu:hover .dropdown-options';
+  let style = 'visibility: visible';
 
-	if ( 'insertRule' in sheet ) {
-		sheet.insertRule(selector + '{' + style + '}', 1);
-	}
-	else if ( 'addRule' in sheet ) {
-		sheet.addRule(selector, style, 1);
-	}
-	*/
-	//el.classList.replace('visible', 'hidden');
+  if ( 'insertRule' in sheet ) {
+    sheet.insertRule(selector + '{' + style + '}', 1);
+  }
+  else if ( 'addRule' in sheet ) {
+    sheet.addRule(selector, style, 1);
+  }
+  */
+  //el.classList.replace('visible', 'hidden');
 
-	//if (jsn.controlPanel.action === 'new') {
-	//	jsn.display.newSong();
-	//}
-	//else if (jsn.controlPanel.action === 'edit') {
-	//	jsn.display.fileBrowser();
-	//}
-	//else if (jsn.controlPanel.action === 'save') {
-	//	jsn.saveSong();
-	//}
+  //if (jsn.controlPanel.action === 'new') {
+  //  jsn.display.newSong();
+  //}
+  //else if (jsn.controlPanel.action === 'edit') {
+  //  jsn.display.fileBrowser();
+  //}
+  //else if (jsn.controlPanel.action === 'save') {
+  //  jsn.saveSong();
+  //}
 }
 
--- client/js/jsn/util/openfile.js
+++ client/js/jsn/util/openfile.js
@@ -24,178 +24,185 @@
 import { jsn } from '../index.js';
 
 export function openFile(song_name) {
-	/*
-	 * This method is called when a file has been selected from the
-	 * file-browser menu as well as from the recently opened file menu option.
-	 */
-	console.log('jsn:/client/js/util/openfile.js:openFile(): song_name =', song_name);
+  /*
+   * This method is called when a file has been selected from the
+   * file-browser menu as well as from the recently opened file menu option.
+   */
+  console.log('jsn:/client/js/util/openfile.js:openFile(): song_name =', song_name);
 
-	if (song_name == '' || song_name == null) {
-		alert('Please select a song to open.');
-		return;
-	}
+  if (song_name == '' || song_name == null) {
+    alert('Please select a song to open.');
+    return;
+  }
 
-	// Make sure the selected song is in local storage
-	// so it can be used by the Recent Files submenu.
-	let recent_songs = [];
+  // Make sure the selected song is in local storage
+  // so it can be used by the Recent Files submenu.
+  let recent_songs = [];
 
-	if (localStorage.getItem('songs') !== null) {
-		recent_songs = JSON.parse(localStorage.getItem('songs'));
-		console.log('jsn:/client/js/jsn/util/openfile.js:openFile:getsong(): number of recent_songs titles in local storage =', recent_songs.length);
-	}
-	if (recent_songs.includes(song_name) === false) {
-		recent_songs.push(song_name);
-	}
-	localStorage.setItem('songs', JSON.stringify(recent_songs));
+  if (localStorage.getItem('songs') !== null) {
+    recent_songs = JSON.parse(localStorage.getItem('songs'));
+    console.log('jsn:/client/js/jsn/util/openfile.js:openFile:getsong(): number of recent_songs titles in local storage =', recent_songs.length);
+  }
+  if (recent_songs.includes(song_name) === false) {
+    recent_songs.push(song_name);
+  }
+  localStorage.setItem('songs', JSON.stringify(recent_songs));
 
-	let recent_files_div = document.getElementById('recent-files-opened');
-	if (recent_files_div.classList.contains('disabled')) {
-		recent_files_div.classList.remove('disabled');
-	}
+  let recent_files_div = document.getElementById('recent-files-opened');
+  if (recent_files_div.classList.contains('disabled')) {
+    recent_files_div.classList.remove('disabled');
+  }
 
-//	let recent_songs = JSON.parse(localStorage.getItem('songs'));
-	//console.log('jsn:/client/js/jsn/events/openfile.js:openFile:getsong(): number of songs titles in local storage =', recent_songs.length);
+//  let recent_songs = JSON.parse(localStorage.getItem('songs'));
+  //console.log('jsn:/client/js/jsn/events/openfile.js:openFile:getsong(): number of songs titles in local storage =', recent_songs.length);
 
-//	for (let ndx=0; ndx<recent_songs.length; ndx++) {
-//		let song_name = recent_songs[ndx];
-		//console.log('JSN:/client/js/index.js:window.onload(): >>> GOT RECENT FILE['+i+'] =', song_name);
+//  for (let ndx=0; ndx<recent_songs.length; ndx++) {
+//    let song_name = recent_songs[ndx];
+    //console.log('JSN:/client/js/index.js:window.onload(): >>> GOT RECENT FILE['+i+'] =', song_name);
 
-		let recent_song_elist = [];
+    let recent_song_elist = [];
 
-		let child_nodes = document.querySelector('.dropdown-submenu').childNodes;
-		if (child_nodes.length) {
-			for (let i=0, ilen=child_nodes.length; i<ilen; ++i) {
-				if (child_nodes[i].childNodes.length) {
-					for (let j=0, jlen=child_nodes[i].childNodes.length; j<jlen; ++j) {
-						console.log('JSN:/client/js/index.js:window.onload(): child_nodes['+i+'] =', child_nodes[i].childNodes[j].nodeValue);
-						recent_song_elist.push(child_nodes[i].childNodes[j].nodeValue);
-					}
-				}
-			}
+    let child_nodes = document.querySelector('.dropdown-submenu').childNodes;
+    if (child_nodes.length) {
+      for (let i=0, ilen=child_nodes.length; i<ilen; ++i) {
+        if (child_nodes[i].childNodes.length) {
+          for (let j=0, jlen=child_nodes[i].childNodes.length; j<jlen; ++j) {
+            console.log('JSN:/client/js/index.js:window.onload(): child_nodes['+i+'] =', child_nodes[i].childNodes[j].nodeValue);
+            recent_song_elist.push(child_nodes[i].childNodes[j].nodeValue);
+          }
+        }
+      }
 
-			if (!recent_song_elist.includes(song_name)) {
-				let li = document.createElement('li');
-				li.className = 'recent-file-item';
-				li.innerText = song_name;
-				li.addEventListener('click', jsn.events.selectSong);
+      if (!recent_song_elist.includes(song_name)) {
+        let li = document.createElement('li');
+        li.className = 'recent-file-item';
+        li.innerText = song_name;
+        li.addEventListener('click', jsn.events.selectSong);
 
-				if (document.querySelector('.dropdown-submenu') !== null) {
-					document.querySelector('.dropdown-submenu').appendChild(li);
-					//console.log('JSN:/client/js/index.js:window.onload(): >>> APPENDED =', li);
-				}
-			}
-		}
-		else {
-			let li = document.createElement('li');
-			li.className = 'recent-file-item';
-			li.innerText = song_name;
-			li.addEventListener('click', jsn.events.selectSong);
+        if (document.querySelector('.dropdown-submenu') !== null) {
+          document.querySelector('.dropdown-submenu').appendChild(li);
+          //console.log('JSN:/client/js/index.js:window.onload(): >>> APPENDED =', li);
+        }
+      }
+    }
+    else {
+      let li = document.createElement('li');
+      li.className = 'recent-file-item';
+      li.innerText = song_name;
+      li.addEventListener('click', jsn.events.selectSong);
 
-			if (document.querySelector('.dropdown-submenu') !== null) {
-				document.querySelector('.dropdown-submenu').appendChild(li);
-				//console.log('JSN:/client/js/index.js:window.onload(): >>> APPENDED =', li);
-			}
-		}
-
-/*
-		if (recent_songs.includes(song_name) === false) {
-			recent_songs.push(song_name);
-			let li = document.createElement('li');
-			li.className = 'recent-file-item';
-			li.innerText = song_name;
-			li.addEventListener('click', jsn.events.selectSong);
-
-			if (document.querySelector('.dropdown-submenu') !== null) {
-				document.querySelector('.dropdown-submenu').appendChild(li);
-				//console.log('JSN:/client/js/index.js:window.onload(): >>> APPENDED =', li);
-			}
-		}
-*/
-//	}
-
-	let recent_file_el = null;
-
-	// The getsong function executes an ajax request for the song.
-	const getsong = async function(url, content_type) {
-		let song = await jsn.dispatch.get(url, content_type);
-		//console.log('jsn:/client/js/jsn/util/openfile.js:openFile:getsong(): song =', song);
-
-		if (song.statusCode != undefined && song.statusCode != 200) {
-			console.log('jsn:/client/js/jsn/util/openfile.js:openFile:getsong(): ERROR: song.statusCode =', song.statusCode);
-			alert(JSON.stringify(song));
-			return;
-		}
-
-		jsn.song = JSON.parse(song);
-		console.log('jsn:/client/js/jsn/util/openfile.js:openFile:getsong(): jsn.song =', jsn.song);
-
-		if (jsn.meta.fileBrowser) {
-			// Hide the file-browser until it's needed again.
-			jsn.meta.fileBrowser.style.visibility = 'hidden';
-		}
-//		else if (recent_file_el !== null) {
-//			if (recent_file_el.classList.contains('selected-song')) {
-//				recent_file_el.classList.remove('selected-song')
-//			}
-//			let parent_classlist = recent_file_el.parentElement.parentElement.parentElement.classList;
-//			if (parent_classlist.contains('visible')) {
-////				parent_classlist.replace('visible', 'hidden')
-//			}
-//		}
-
-		// Now that we have a song we can turn on the Print file menu option.
-		if (document.getElementById('print-menu-option') !== null) {
-			if (document.getElementById('print-menu-option').classList.contains('disabled')) {
-				document.getElementById('print-menu-option').classList.toggle('disabled');
-			}
-		}
-
-		// And the view menu options.
-		const view_menu_items = document.querySelectorAll('.view-menu-item');
-		for (let i=0, len=view_menu_items.length; i<len; ++i) {
-			view_menu_items[i].classList.toggle('disabled');
-			if (view_menu_items[i].classList.contains('disabled')) {
-				view_menu_items[i].classList.toggle('disabled');
-			}
-			view_menu_items[i].addEventListener('click', jsn.events.setView);
-		}
-
-		// Now that we have a song we can turn on the metronome.
-		if (document.getElementById('tools-metronome') !== null) {
-			if (document.querySelector('#tools-metronome').classList.contains('disabled')) {
-				document.getElementById('tools-metronome').classList.toggle('disabled');
-			}
-		}
-
-		// Now that we have a song we can turn on the save file option.
-		if (document.getElementById('save-menu-option') !== null) {
-			if (document.getElementById('save-menu-option').classList.contains('disabled')) {
-				document.getElementById('save-menu-option').classList.toggle('disabled');
-			}
-		}
-
-		// This to close the menu.
-		if (document.querySelector('.dropdown-submenu') !== null) {
-			if (document.querySelector('.dropdown-submenu').classList.contains('show')) {
-				document.querySelector('.dropdown-submenu').classList.remove('show');
-				document.querySelector('.dropdown-submenu').classList.add('hide');
-			}
-		}
+      if (document.querySelector('.dropdown-submenu') !== null) {
+        document.querySelector('.dropdown-submenu').appendChild(li);
+        //console.log('JSN:/client/js/index.js:window.onload(): >>> APPENDED =', li);
+      }
+    }
 
 /*
-		if (document.querySelector('.dropdown-menu') !== null) {
-			if (document.querySelector('.dropdown-menu').classList.contains('show')) {
-				document.querySelector('.dropdown-menu').classList.remove('show');
-				document.querySelector('.dropdown-menu').classList.add('hide');
-			}
-		}
+    if (recent_songs.includes(song_name) === false) {
+      recent_songs.push(song_name);
+      let li = document.createElement('li');
+      li.className = 'recent-file-item';
+      li.innerText = song_name;
+      li.addEventListener('click', jsn.events.selectSong);
+
+      if (document.querySelector('.dropdown-submenu') !== null) {
+        document.querySelector('.dropdown-submenu').appendChild(li);
+        //console.log('JSN:/client/js/index.js:window.onload(): >>> APPENDED =', li);
+      }
+    }
+*/
+//  }
+
+  let recent_file_el = null;
+
+  // The getsong function executes an ajax request for the song.
+  const getsong = async function(url, content_type) {
+    let song = await jsn.dispatch.get(url, content_type);
+    //console.log('jsn:/client/js/jsn/util/openfile.js:openFile:getsong(): song =', song);
+
+    if (song.statusCode != undefined && song.statusCode != 200) {
+      console.log('jsn:/client/js/jsn/util/openfile.js:openFile:getsong(): ERROR: song.statusCode =', song.statusCode);
+      alert(JSON.stringify(song));
+      return;
+    }
+
+    jsn.song = JSON.parse(song);
+    console.log('jsn:/client/js/jsn/util/openfile.js:openFile:getsong(): jsn.song =', jsn.song);
+
+    if (jsn.meta.fileBrowser) {
+      // Hide the file-browser until it's needed again.
+      jsn.meta.fileBrowser.style.visibility = 'hidden';
+    }
+//    else if (recent_file_el !== null) {
+//      if (recent_file_el.classList.contains('selected-song')) {
+//        recent_file_el.classList.remove('selected-song')
+//      }
+//      let parent_classlist = recent_file_el.parentElement.parentElement.parentElement.classList;
+//      if (parent_classlist.contains('visible')) {
+////        parent_classlist.replace('visible', 'hidden')
+//      }
+//    }
+
+    // Now that we have a song we can turn on the Print file menu option.
+    if (document.getElementById('print-menu-option') !== null) {
+      if (document.getElementById('print-menu-option').classList.contains('disabled')) {
+        document.getElementById('print-menu-option').classList.toggle('disabled');
+      }
+    }
+
+    // Now that we have a song we can turn on the Export file menu option.
+    if (document.getElementById('export-menu-option') !== null) {
+      if (document.getElementById('export-menu-option').classList.contains('disabled')) {
+        document.getElementById('export-menu-option').classList.toggle('disabled');
+      }
+    }
+
+    // And the view menu options.
+    const view_menu_items = document.querySelectorAll('.view-menu-item');
+    for (let i=0, len=view_menu_items.length; i<len; ++i) {
+      view_menu_items[i].classList.toggle('disabled');
+      if (view_menu_items[i].classList.contains('disabled')) {
+        view_menu_items[i].classList.toggle('disabled');
+      }
+      view_menu_items[i].addEventListener('click', jsn.events.setView);
+    }
+
+    // Now that we have a song we can turn on the metronome.
+    if (document.getElementById('tools-metronome') !== null) {
+      if (document.querySelector('#tools-metronome').classList.contains('disabled')) {
+        document.getElementById('tools-metronome').classList.toggle('disabled');
+      }
+    }
+
+    // Now that we have a song we can turn on the save file option.
+    if (document.getElementById('save-menu-option') !== null) {
+      if (document.getElementById('save-menu-option').classList.contains('disabled')) {
+        document.getElementById('save-menu-option').classList.toggle('disabled');
+      }
+    }
+
+    // This to close the menu.
+    if (document.querySelector('.dropdown-submenu') !== null) {
+      if (document.querySelector('.dropdown-submenu').classList.contains('show')) {
+        document.querySelector('.dropdown-submenu').classList.remove('show');
+        document.querySelector('.dropdown-submenu').classList.add('hide');
+      }
+    }
+
+/*
+    if (document.querySelector('.dropdown-menu') !== null) {
+      if (document.querySelector('.dropdown-menu').classList.contains('show')) {
+        document.querySelector('.dropdown-menu').classList.remove('show');
+        document.querySelector('.dropdown-menu').classList.add('hide');
+      }
+    }
 */
 
-		jsn.display.jsnFormat();
-		console.log('jsn:/client/js/jsn/util/openfile.js:openFile:getsong(): >>> jsn.song =', jsn.song);
-	}
+    jsn.display.jsnFormat();
+    console.log('jsn:/client/js/jsn/util/openfile.js:openFile:getsong(): >>> jsn.song =', jsn.song);
+  }
 
-	let url = '/api/1.0/getsong/' + song_name;
-	getsong(url, 'json');
+  let url = '/api/1.0/getsong/' + song_name;
+  getsong(url, 'json');
 }
 
