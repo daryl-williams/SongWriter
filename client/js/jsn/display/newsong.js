69d78894accd092c1d972cb30b90a513a37fa5ee
Daryl Williams <daryl@weblane.com>
2020-06-21 13:49:56 -0700

    Transitioning to using spaces instead of tabs for indentation
    Also still testing automatic update of about.html file.

--- client/js/jsn/display/newsong.js
+++ client/js/jsn/display/newsong.js
@@ -24,82 +24,82 @@
 import { jsn } from '../../jsn/index.js';
 
 export function newSong() {
-	// The purpose of this function is to present a clean
-	// metadata form so as to begin creating a new song.
-	//console.log('jsn:/client/js/jsn/display/newsong.js:newSong(): jsn.song =', jsn.song);
+  // The purpose of this function is to present a clean
+  // metadata form so as to begin creating a new song.
+  //console.log('jsn:/client/js/jsn/display/newsong.js:newSong(): jsn.song =', jsn.song);
 
-	jsn.meta.action = 'edit';
+  jsn.meta.action = 'edit';
 
-	// Now that we have a song we can turn on the Save file option.
-	if (document.getElementById('save-menu-option') !== null) {
-		document.getElementById('save-menu-option').classList.add('disabled');
-	}
+  // Now that we have a song we can turn on the Save file option.
+  if (document.getElementById('save-menu-option') !== null) {
+    document.getElementById('save-menu-option').classList.add('disabled');
+  }
 
-	// We also need to disable the File Print menu option.
-	if (document.getElementById('print-menu-option') !== null) {
-		document.getElementById('print-menu-option').classList.add('disabled');
-	}
+  // We also need to disable the File Print menu option.
+  if (document.getElementById('print-menu-option') !== null) {
+    document.getElementById('print-menu-option').classList.add('disabled');
+  }
 
-	// We also need to disable the View menu Toggle Console option.
-	if (document.getElementById('viewmenu-toggle-console') !== null) {
-		document.getElementById('viewmenu-toggle-console').classList.add('disabled');
-	}
+  // We also need to disable the View menu Toggle Console option.
+  if (document.getElementById('viewmenu-toggle-console') !== null) {
+    document.getElementById('viewmenu-toggle-console').classList.add('disabled');
+  }
 
-	// We also need to disable the View menu Display option.
-	if (document.getElementById('display-song') !== null) {
-		document.getElementById('display-song').classList.add('disabled');
-	}
+  // We also need to disable the View menu Display option.
+  if (document.getElementById('display-song') !== null) {
+    document.getElementById('display-song').classList.add('disabled');
+  }
 
-	// We also need to disable the View menu Preview option.
-	if (document.getElementById('preview-song') !== null) {
-		document.getElementById('preview-song').classList.add('disabled');
-	}
+  // We also need to disable the View menu Preview option.
+  if (document.getElementById('preview-song') !== null) {
+    document.getElementById('preview-song').classList.add('disabled');
+  }
 
-	// Clear out any previous song header data.
-	for (let prop in jsn.song.header) {
-		if (jsn.song.header.hasOwnProperty(prop)) {
-			jsn.song.header[prop] = '';
-			console.log('jsn:/client/js/jsn/display/newsong.js:newSong(): song header[' +  prop + '] =', jsn.song.header[prop]);
-		}
-	}
+  // Clear out any previous song header data.
+  for (let prop in jsn.song.header) {
+    if (jsn.song.header.hasOwnProperty(prop)) {
+      jsn.song.header[prop] = '';
+      console.log('jsn:/client/js/jsn/display/newsong.js:newSong(): song header[' +  prop + '] =', jsn.song.header[prop]);
+    }
+  }
 
-	jsn.song.body = [];
+  jsn.song.body = [];
 
-	if (document.getElementById('song-metadata-form') !== null) {
-		const form = document.getElementById('song-metadata-form');
-		[...form.elements].forEach((input) => {
-				console.log('jsn:/client/js/jsn/display/newsong.js:newSong(): form input =', input.id);
-				if (input.tagName === 'INPUT') {
-					input.value = '';
-				}
-				else if (input.tagName === 'SELECT') {
-					input.options[0].defaultSelected = true;
-				}
-		});
-	}
+  if (document.getElementById('song-metadata-form') !== null) {
+    const form = document.getElementById('song-metadata-form');
+    [...form.elements].forEach((input) => {
+        console.log('jsn:/client/js/jsn/display/newsong.js:newSong(): form input =', input.id);
+        if (input.tagName === 'INPUT') {
+          input.value = '';
+        }
+        else if (input.tagName === 'SELECT') {
+          input.options[0].defaultSelected = true;
+        }
+    });
+  }
 
-	if (document.getElementById('app-console') !== null) {
-		document.getElementById('app-console').classList.replace('hide', 'visible');
-	}
+  if (document.getElementById('app-console') !== null) {
+    document.getElementById('app-console').classList.replace('hide', 'visible');
+  }
 
-	if (document.getElementById('song-content') !== null) {
-		document.getElementById('song-content').innerHTML = '<div id="song-grid"></div>';
-	}
+  if (document.getElementById('song-content') !== null) {
+    document.getElementById('song-content').innerHTML = '<div id="song-grid"></div>';
+  }
 
-	if (document.getElementById('song-grid') !== null) {
-		document.getElementById('song-grid').innerHTML = '';
-	}
+  if (document.getElementById('song-grid') !== null) {
+    document.getElementById('song-grid').innerHTML = '';
+  }
 
-	if (document.getElementById('chord-family') !== null) {
-		document.getElementById('chord-family').innerHTML = '';
-	}
+  if (document.getElementById('chord-family') !== null) {
+    document.getElementById('chord-family').innerHTML = '';
+  }
 
-	if (document.getElementById('chords-additional') !== null) {
-		document.getElementById('chords-additional').innerHTML = '';
-	}
+  if (document.getElementById('chords-additional') !== null) {
+    document.getElementById('chords-additional').innerHTML = '';
+  }
 
-	if (document.getElementById('viewmenu-toggle-console') !== null) {
-		document.getElementById('viewmenu-toggle-console').classList.toggle('disabled');
-	}
+  if (document.getElementById('viewmenu-toggle-console') !== null) {
+    document.getElementById('viewmenu-toggle-console').classList.toggle('disabled');
+  }
 }
 
