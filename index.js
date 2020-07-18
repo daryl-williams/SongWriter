1eed7163646b11e66a1663f76974b4e97f851ad4
Daryl Williams <daryl@weblane.com>
2020-07-18 12:33:02 -0700

    Enclosed dotenv require in a conditional for development
    Heroku crashed with an error about dotenv module not found.
    Heroku docs says to inject env vars directly to Heroku using
    the consig:set command, i.e. $ heroku config:set SOME_VAR.

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
  * /app/index.js
  */
 
-require('dotenv').config()
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config()
+}
 
 const express = require("express");
 const app = express();
